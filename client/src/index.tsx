e917f6c7ecdf8c34ca5b3ef09331a5d82472e322
davehastings <david@two.fish>
2022-02-13 11:11:19 -0500

    add react, redux, typescript and create basic counter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './state/store';
+import { Provider } from "react-redux"
+import {store} from "./state/index";
 
 ReactDOM.render(
   <React.StrictMode>
